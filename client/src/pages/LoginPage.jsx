ec19e43776d0d7bc8787ac289b55882b72239009
Mohamed Gamal <mohamed.g.shoaib@gmail.com>
2025-06-16 02:04:05 +0300

    fix: change api urls to backend url

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -33,7 +33,7 @@ const LoginPage = () => {
   const onSubmit = async (formData) => {
     setIsLoading(true);
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/login', formData);
+      const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/login`, formData);
 
       if (response.data.success) {
         // Use login function from AuthContext
